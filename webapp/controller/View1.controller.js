1d3d4ecaea91d85910648621f714e3a9eda557ac
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-01-30 19:45:57 +0530

    Updated View and Controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,9 +1,30 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	'sap/m/Button',
+	'sap/m/Dialog'
+], function(Controller,Dialog,Button) {
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-11_ListControll.controller.View1", {
+		
+		// 	handlePress: function (evt) {
+		// 	var sSrc = evt.getSource().getTarget();
+		// 	var oDialog = new Dialog({
+		// 		content: new Image({
+		// 			src: sSrc
+		// 		}),
+		// 		beginButton: new Button({
+		// 			text: 'Close',
+		// 			press: function () {
+		// 				oDialog.close();
+		// 			}
+		// 		}),
+		// 		afterClose: function() {
+		// 			oDialog.destroy();
+		// 		}
+		// 	});
+		// 	oDialog.open();
+		// }
 
 	});
 });
