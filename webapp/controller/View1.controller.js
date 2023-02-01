5950dabc9127bf93a6ee30c88fd530543e1ac5b6
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-01 19:05:45 +0530

    solve error in custom list

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -2,29 +2,28 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	'sap/m/Button',
 	'sap/m/Dialog'
-], function(Controller,Dialog,Button) {
+], function(Controller,Button, Dialog) {
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-11_ListControll.controller.View1", {
 		
-		// 	handlePress: function (evt) {
-		// 	var sSrc = evt.getSource().getTarget();
-		// 	var oDialog = new Dialog({
-		// 		content: new Image({
-		// 			src: sSrc
-		// 		}),
-		// 		beginButton: new Button({
-		// 			text: 'Close',
-		// 			press: function () {
-		// 				oDialog.close();
-		// 			}
-		// 		}),
-		// 		afterClose: function() {
-		// 			oDialog.destroy();
-		// 		}
-		// 	});
-		// 	oDialog.open();
-		// }
+			handlePress: function (evt) {
+			var sSrc = evt.getSource().getTarget();
+			var oDialog = new Dialog({
+				content: new sap.m.Image({
+					src: sSrc				}),
+				beginButton: new Button({
+					text: 'Close',
+					press: function () {
+						oDialog.close();
+					}
+				}),
+				afterClose: function() {
+					oDialog.destroy();
+				}
+			});
+			oDialog.open();
+		}
 
 	});
 });
